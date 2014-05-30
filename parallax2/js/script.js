e0e8b54469ed2c286d1e9689972be0fcaf8fcf1d
Kyle Meier <kyledmeier@gmail.com>
2014-05-29 21:11:57 -0700

    completed turtle and cat

--- parallax2/js/script.js
+++ parallax2/js/script.js
@@ -1,7 +1,7 @@
 (function(){ 
 $ (function(){
   var firstVar = 0,
-      properties = ['translateX','translateY', 'opacity', 'rotate', 'scale', 'skew'];
+      properties = ['translateX','translateY', 'opacity', 'rotate', 'scaleXX', 'skew'];
       $doc = $(document),
       $window = $(window),
       $body = $('body'),
@@ -102,71 +102,34 @@ $ (function(){
               skew: [-45,-45]  
             }     
           ]
-        }//,{
-        //   wrapper: '#tangram',
-        //   duration: 100,
-        //   animations: [
-        //     {
-        //       element: '.square',
-        //       translateY: [6,6],
-        //       translateX: [2,2],
-        //       rotate: [45,45]
-        //     },{
-        //       element: '.triangle-small-1',
-        //       translateY: [8,8],
-        //       translateX: [3,3],
-        //       rotate: [-90,-90]
-        //     },{
-        //       element: '.triangle-small-2',
-        //       translateY: [9,9]
-        //     },{
-        //       element: '.triangle-medium',
-        //       translateY: [10,10],
-        //       translateX: [3,3],
-        //       rotate: [135,135]
-        //     },{
-        //       element: '.triangle-large-1',
-        //       translateY: [7,7],
-        //       rotate: [180,180]
-        //     },{
-        //       element: '.triangle-large-2',
-        //       translateY: [10,10],
-        //       translateX: [-3,-3],
-        //       rotate: [90,90]
-        //     },{
-        //       element: '.parallelogram',
-        //       translateY: [10,10],
-        //       skew: [-45,-45]  
-        //     }     
-        //   ]
-        // },
-          ,{
+        },{
           wrapper: '#tangram',
           duration: 1000,
           animations: [
             {
               element: '.square',
               translateY: [9.5,7],
-              translateX: [-1.5,1],
+              translateX: [-1.5,1.1],
               rotate: [45,45]
             },{
               element: '.triangle-small-1',
-              translateY: [5,6],
-              translateX: 2,
-              rotate: [-90,-90]
+              translateY: [6,12.9],
+              translateX: [2, -1.1],
+              rotate: [-90,100]
             },{
               element: '.triangle-small-2',
-              translateY: [5,7.5],
-              translateX: -5.5
+              translateY: [7.5,8],
+              translateX: [-5.5, -2.7],
+              rotate: -10
             },{
               element: '.triangle-medium',
-              translateY: [5,7],
-              translateX: 1,
-              rotate: [135,135]
+              translateY: [7,4.7],
+              translateX: [1,-2.8],
+              rotate: [135,250]
             },{
               element: '.triangle-large-1',
-              translateY: [8,11],
-              translateX: 1,
+              translateY: [8,10.9],
+              translateX: .9,
               rotate: [180,135]
             },{
               element: '.triangle-large-2',
@@ -175,9 +138,138 @@ $ (function(){
               rotate: [90,-45]
             },{
               element: '.parallelogram',
-              translateY: [5,8],
-              translateX: -6,
-              skew: [-45,-45]  
+              translateY: [8,7.3],
+              translateX: [-6, 3.9],
+              rotate: 5,
+              skew: [-45,-45]
+            }     
+          ]
+        },{
+          wrapper: '#tangram',
+          duration: 200,
+          animations: [
+            {
+              element: '.square',
+              translateY: [7,7],
+              translateX: [1.1,1.1],
+              rotate: [45,45]
+            },{
+              element: '.triangle-small-1',
+              translateY: [12.9,12.9],
+              translateX: [-1.1, -1.1],
+              rotate: [100,100]
+            },{
+              element: '.triangle-small-2',
+              translateY: [8,8],
+              translateX: [-2.7, -2.7],
+              rotate: [-10,-10]
+            },{
+              element: '.triangle-medium',
+              translateY: [4.7,4.7],
+              translateX: [-2.8,-2.8],
+              rotate: [250,250]
+            },{
+              element: '.triangle-large-1',
+              translateY: [10.9,10.9],
+              translateX: [.9,.9],
+              rotate: [135,135]
+            },{
+              element: '.triangle-large-2',
+              translateY: [8.3,8.3],
+              translateX: [.4,.4],
+              rotate: [-45,-45]
+            },{
+              element: '.parallelogram',
+              translateY: [7.3,7.3],
+              translateX: [3.9, 3.9],
+              rotate: [5, 5],
+              skew: [-45,-45]
+            }     
+          ]
+        },{
+          wrapper: '#tangram',
+          duration: 1000,
+          animations: [
+            {
+              element: '.square',
+              translateY: [7,10],
+              translateX: [1.1,-4],
+              rotate: [45,45]
+            },{
+              element: '.triangle-small-1',
+              translateY: [12.9,10],
+              translateX: [-1.1, -4],
+              rotate: [100,-90]
+            },{
+              element: '.triangle-small-2',
+              translateY: [8,8.4],
+              translateX: [-2.7, -3.5],
+              rotate: [-10,90]
+            },{
+              element: '.triangle-medium',
+              translateY: [4.7,9.5],
+              translateX: [-2.8,-2.3],
+              rotate: [250,270]
+            },{
+              element: '.triangle-large-1',
+              translateY: [10.9,15],
+              translateX: [.9,1.4],
+              rotate: [135,135]
+            },{
+              element: '.triangle-large-2',
+              translateY: [8.3,11.7],
+              translateX: [.4,2.15],
+              rotate: [-45,90]
+            },{
+              element: '.parallelogram',
+              translateY: [7.3,8.75],
+              translateX: [3.9, -.95],
+              rotate: [5, 45],
+              scaleX: -1,
+              skew: [-45,-45]
+            }     
+          ]
+        },{
+          wrapper: '#tangram',
+          duration: 200,
+          animations: [
+            {
+              element: '.square',
+              translateY: [10,10],
+              translateX: [-4,-4],
+              rotate: [45,45]
+            },{
+              element: '.triangle-small-1',
+              translateY: [10,10],
+              translateX: [-4, -4],
+              rotate: [-90,-90]
+            },{
+              element: '.triangle-small-2',
+              translateY: [8.4,8.4],
+              translateX: [-3.5, -3.5],
+              rotate: [90,90]
+            },{
+              element: '.triangle-medium',
+              translateY: [9.5,9.5],
+              translateX: [-2.3,-2.3],
+              rotate: [270,270]
+            },{
+              element: '.triangle-large-1',
+              translateY: [15,15],
+              translateX: [1.4,1.4],
+              rotate: [135,135]
+            },{
+              element: '.triangle-large-2',
+              translateY: [11.7,11.7],
+              translateX: [2.15,2.15],
+              rotate: [90,90]
+            },{
+              element: '.parallelogram',
+              translateY: [8.75,8.75],
+              translateX: [-.95, -.95],
+              rotate: [45, 45],
+              scaleX: [-1, -1],
+              skew: [-45,-45]
             }     
           ]
         },{
@@ -209,7 +301,7 @@ init = function(){
         return 1;
       case 'rotate':
         return 0;
-      case 'scale':
+      case 'scaleX':
         return 1;
       case 'skew':
         return 0;
@@ -296,18 +388,18 @@ setScrollTops = function(){
 }
 
 animateElements = function(){
-  var animation, translateY, translateX, scale, rotate, skew, opacity;
+  var animation, translateY, translateX, scaleX, rotate, skew, opacity;
   for(var i = 0; i<keyframes[currentKeyframe].animations.length; i++){
     animation = keyframes[currentKeyframe].animations[i];
     translateY = calcValue(animation, 'translateY');
     translateX = calcValue(animation, 'translateX');     
-    scale = calcValue(animation, 'scale');
+    scaleX = calcValue(animation, 'scaleX');
     rotate  = calcValue(animation, 'rotate');
     skew = calcValue(animation, 'skew');
     opacity = calcValue(animation, 'opacity');
 
     $(animation.element).css({
-      'transform': 'translate3d(' + translateX + 'em, ' + translateY + 'em, 0) scale(' + scale + ') rotate(' + rotate + 'deg) skew('+ skew +'deg)',
+      'transform': 'translate3d(' + translateX + 'em, ' + translateY + 'em, 0) scaleX(' + scaleX + ') rotate(' + rotate + 'deg) skew('+ skew +'deg)',
       'opacity' : opacity 
     })
   }
@@ -321,7 +413,7 @@ animateElements = function(){
         value = getDefaultPropertyValue(property);
       }
       // value = +value.toFixed(2) 
-      // TEMPORARILY REMOVED CAUSE SCALE DOESN'T WORK WITHA AGRESSIVE ROUNDING LIKE THIS
+      // TEMPORARILY REMOVED CAUSE scaleX DOESN'T WORK WITHA AGRESSIVE ROUNDING LIKE THIS
       return value;
     }
 
